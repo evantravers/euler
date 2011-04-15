c51cd3eaf7db04e333da520d5be8379382f5a407
evantravers <widgetwebworks@gmail.com>
2011-04-14 22:28:32 -0500

    working on 23

--- python/23.py
+++ python/23.py
@@ -1,11 +1,23 @@
+import math
+import operator
+
 # problem 23
 # find the sum of the all positive integers which cannot be written as the sum of two abundant numbers
 
+def abundant(num):
+	"""this function will find the proper divisors of a number"""
+	factors = []
+	for factor in reversed(xrange(1, num)):
+		if num % factor == 0:
+				factors.append(factor)
+	return reduce( operator.add, factors) > num 
+		
 isAbundant = []
-for each in xrange(0, 28124):
+for each in xrange(2, 28124):
+	if each % 100 == 0:
+		print each
 	# check and see if it's abundant
-	if each % 2 == 0:
-		isAbundant.append(True)
-	else:
-		isAbundant.append(False)
+	# abundant means that it's proper divisors added together exceeds the number itself
+	isAbundant.append(abundant(each))
 
+print isAbundant
