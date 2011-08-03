2bbf4430c2f39ec15961f928377e2d60810740c4
Evan Travers <widgetwebworks@gmail.com>
2011-08-03 16:28:33 -0500

    added comments to 35 for clarification

--- ruby/35.rb
+++ ruby/35.rb
@@ -28,15 +28,22 @@ class Sieve
   end
 end
 
+# generate all the primes
 s = Sieve.new(1000000)
 poss = s.primes
 
 sum = 0
+# for each prime
 poss.each do |prime|
+  # convert it to an array for rotations
   str = prime.to_s.chars.to_a
   bad = false
+  # ignore it if it contains any even numbers, because a rotation ending in an even number will not be a prime
   next if str.include?("0") | str.include?("2") | str.include?("4") | str.include?("6") | str.include?("8")
   for i in (0..str.size)
+    # check to see if the rotation is in the big list o' primes
+    # this is oodles faster than my original prime checker
+    # this alone took it from 59s to 6s
     if not s[str.rotate!.inject(:+).to_i]
       bad = true
       break
