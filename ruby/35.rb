c0fe43477236726f588c92dfe6eabe09d98f9b78
Evan Travers <widgetwebworks@gmail.com>
2011-08-03 16:47:10 -0500

    some more optimizations on 35.rb

--- ruby/35.rb
+++ ruby/35.rb
@@ -12,7 +12,7 @@ class Sieve
     (2..(n**0.5)+1).each do |num|
       (2..@sieve.size/num).each do |divisor|
         @sieve[num*divisor] = nil
-        # puts "#{sieve[num*divisor]==nil} => #{num} x #{divisor} = #{num*divisor} :: #{sieve.to_s}\n"
+        # puts '#{sieve[num*divisor]==nil} => #{num} x #{divisor} = #{num*divisor} :: #{sieve.to_s}\n'
       end
     end
     puts "...done!"
@@ -39,16 +39,16 @@ poss.each do |prime|
   str = prime.to_s.chars.to_a
   bad = false
   # ignore it if it contains any even numbers, because a rotation ending in an even number will not be a prime
-  next if str.include?("0") | str.include?("2") | str.include?("4") | str.include?("6") | str.include?("8")
+  next if str.include?('0') || str.include?('2') || str.include?('4') || str.include?('6') || str.include?('8')
   for i in (0..str.size)
     # check to see if the rotation is in the big list o' primes
     # this is oodles faster than my original prime checker
     # this alone took it from 59s to 6s
-    if not s[str.rotate!.inject(:+).to_i]
+    if !s[str.rotate!.inject(:+).to_i]
       bad = true
       break
     end
   end
-  sum+=1 if not bad
+  sum += 1 unless bad
 end
 puts sum
