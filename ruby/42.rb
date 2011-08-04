9221e2ea0494b3b7ff6af913182c95fb01c5033d
Evan Travers <widgetwebworks@gmail.com>
2011-08-04 11:49:41 -0500

    I have a working solution for 42, first time.
    Good times had by all.

--- ruby/42.rb
+++ ruby/42.rb
@@ -1,3 +1,4 @@
+require 'set'
 # build the hash for the values
 values = {}
 'a'.upto('z').each { |v| values[v] = values.size+1 }
@@ -14,9 +15,24 @@ end
 
 # definition of triangle numbers
 def triangle(n)
-  0.5*n(n+1)
+  (0.5*n*(n+1)).to_i
 end
 
+# lets build a set of triangle numbers!
+tri = Set.new
+(0..1000).each do |num|
+  tri.add(triangle(num))
+end
+
+result = 0
 arr.each do |word|
-
+  sum = 0
+  word.each_char do |char|
+    sum += values[char]
+  end
+  if tri.include?(sum)
+    result +=1
+  end
 end
+
+puts result
