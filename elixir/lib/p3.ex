871107d4afb751cfa87e61cd361eb350775a3c80
Evan Travers <evantravers@gmail.com>
2018-08-21 12:28:14 -0500

    Documenting and cleaning up P3

--- elixir/lib/p3.ex
+++ elixir/lib/p3.ex
@@ -1,12 +1,19 @@
 defmodule Euler.P3 do
   @moduledoc "https://projecteuler.net/problem=3"
 
+  @doc """
+  Simple sieve of eratosthenes. I don't think it benefits from the one array
+  speedup of the normal declarative version... unless elixir/erlang is sharing
+  the memory space of the list being passed around.
+
+  Also, the use of `rem()` instead of cheating with the array index trick is
+  hurting me here. I may try a speedup at some point, unsure what that would
+  look like in elixir.
+  """
   def sieve(n) when is_integer(n), do: sieve(Enum.to_list(2..n), [])
   def sieve([], result), do: Enum.reverse(result)
   def sieve([prime|tail], result) do
-    tail
-    |> Enum.reject(& rem(&1, prime) == 0)
-    |> sieve([prime|result])
+    tail |> Enum.reject(& rem(&1, prime) == 0) |> sieve([prime|result])
   end
 
   @doc ~S"""
